6ffa4949b0857fab5475d27c9994537e7ff7f80a
Gabriel Demes <gabrieldemes@nyu.edu>
2020-11-24 00:28:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,22 @@
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def toadstool
   puts status
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  Mario
+  toadstool
+  link
+end
+  
